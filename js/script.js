a23778a5efc4c92092d242eb24d6f39d04286364
Xenon3099 <dmamritesh@gmail.com>
2019-08-28 17:32:33 +0530

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,8 @@
+
+$(function(){
+	$("#about-me").typed({
+		strings: ["Student at IIIT-NR.","Member of The Society of Coders.","Natural Language Processing.","Open Source Enthusiast.","Interested in Machine Learning and AI.","Loves to Code <&#x2764>."],
+		typeSpeed: 30,
+		loop: true,
+	});
+});
