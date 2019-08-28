7f69fb7acb588b780a89011e7a29bd22223b4e6c
Xenon3099 <dmamritesh@gmail.com>
2019-08-28 19:14:49 +0530

    update

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 
 $(function(){
 	$("#about-me").typed({
-		strings: ["Student at VIT-AP.","Member of L.I.T. D.A.C.","Member of Android Club.","Senior Member at XDA Forums.","Network Security Enthusiast.","Interested in Machine Learning and AI."],
-		typeSpeed: 30,
+		strings: ["Student at VIT-AP.","Member of Data Analytics Club.","Member of Android Club.","Senior Member at XDA Forums.","Network Security Enthusiast.","Interested in Machine Learning and AI."],
+		typeSpeed: 40,
 		loop: true,
 	});
 });
