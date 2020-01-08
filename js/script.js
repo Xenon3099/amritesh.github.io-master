3e52a381af195e7bf0f3a27207f56bfa118335f2
Amritesh <dmamritesh@gmail.com>
2020-01-08 18:45:41 +0530

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 
 $(function(){
 	$("#about-me").typed({
-		strings: ["Student at VIT-AP.","Member of Android Club.","Senior Member at XDA Forums.","Network Security Enthusiast.","Interested in Android OS Development.","Learning Flutter and React Native.","Strong supporter of Open Source Initiative.","Kali Linux is my daily driver."],
+		strings: ["Student at VIT-AP.","Member of Android Club.","Senior Member at XDA Forums.","Network Security Enthusiast.","Interested in Android OS Development.","Learning Flutter and React Native.","Supporter of Open Source Initiative.","Kali Linux is my daily driver."],
 		typeSpeed: 40,
 		loop: true,
 	});
