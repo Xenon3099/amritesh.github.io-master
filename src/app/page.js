c129c47ac24b765ea9da52eb9df42ab1b215ae62
Amritesh Dasari <dmamritesh@gmail.com>
2023-12-26 12:21:26 -0600

    navbar WIP

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,11 @@
 import Image from 'next/image'
 import HeroSection from './Components/HeroSection'
+import NavBar from './Components/NavBar'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
+      <NavBar/>
       <div className='container mx-auto px-12 py-4'>
       <HeroSection/>
       </div>
