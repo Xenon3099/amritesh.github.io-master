547c4868f3516285b0a0502e4117318b81105550
Amritesh Dasari <dmamritesh@gmail.com>
2023-12-25 22:13:31 -0600

    Yeet the trash out
    Alright lets do this one more time, My name is Amritesh and up until 2 days ago I didn't realize I needed to update this and fix it.

--- js/script.js
+++
@@ -1,8 +0,0 @@
-
-$(function(){
-	$("#about-me").typed({
-		strings: ["Student at VIT-AP.","Head of Operations at Android Club.","Senior Member at XDA Forums.","Network Security Enthusiast.","Interested in Android OS Development.","Learning Flutter and React Native.","Supporter of Open Source Initiative.","Kali Linux is my daily driver."],
-		typeSpeed: 40,
-		loop: true,
-	});
-});
---
+++ src/app/Components/HeroSection.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <section>
+        <h1 className='text-white mb-4 text-6xl font-extrabold'>Hello, I'm Amritesh</h1>
+        <p className='text-[#ADB7BE]'>
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eius mollitia amet cupiditate reprehenderit, perferendis animi cumque quaerat necessitatibus voluptatem quia, quibusdam sed aspernatur itaque neque deserunt. Alias, veritatis. Ea, ipsa!
+        </p>
+    </section>
+  )
+}
+
+export default HeroSection
