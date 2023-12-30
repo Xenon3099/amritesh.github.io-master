f765d673d7f4eb5f6ee183c7829cdb0c068745b8
Amritesh Dasari <dmamritesh@gmail.com>
2023-12-30 17:39:11 -0600

    Code Linting issues fix

--- src/app/Components/HeroSection.jsx
+++ src/app/Components/HeroSection.jsx
@@ -10,7 +10,7 @@ const HeroSection = () => {
       <div className='grid grid-cols-1 sm:grid-cols-12'>
         <div className='col-span-7 place-self-center text-center sm:text-left'>
           <h1 className='text-white mb-4 text-4xl sm:text-5xl lg:text-6xl font-extrabold'>
-            <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-400 via-purple-400 to-red-400'>Hello, I'm{" "}</span>
+            <span className='text-transparent bg-clip-text bg-gradient-to-r from-blue-400 via-purple-400 to-red-400'>Hello, I&apos;m{" "}</span>
             <br />
             <TypeAnimation
               sequence={[
@@ -29,7 +29,7 @@ const HeroSection = () => {
             />
           </h1>
           <p className='text-[#ADB7BE] text-base sm:text-lg mt-4 mb-6 lg:text-xl'>
-            I'm ingesting new knowledge and leveling up my skills by pulling lessons asynchronously from the Kafka queue of experience.
+            I&apos;m ingesting new knowledge and leveling up my skills by pulling lessons asynchronously from the Kafka queue of experience.
           </p>
           <div>
             <Link href="mailto:dmamritesh@gmail.com" className='px-6 py-3 w-full sm:w-fit rounded-full mr-4 bg-gradient-to-br from-blue-500 via-purple-500 to-red-500 hover:bg-slate-200 text-white'>
