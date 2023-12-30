d9dc7c784125be27c067d981371279f543414635
Amritesh Dasari <dmamritesh@gmail.com>
2023-12-30 17:53:07 -0600

    Fixing title properly

--- src/app/page.js
+++ src/app/page.js
@@ -8,11 +8,6 @@ import EmailSection from './Components/EmailSection'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col bg-[#121212]">
-      <div>
-        <head>
-          <title>Amritesh&apos;s Portfolio</title>
-        </head>
-      </div>
       {/* <NavBar /> */}
       <div className='container mt-24 mx-auto px-12 py-4'>
         <HeroSection />
