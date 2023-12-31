d9954f4be08a84ca33c95932699b77689cb930f7
Amritesh Dasari <dmamritesh@gmail.com>
2023-12-30 18:09:53 -0600

    idek what I am doing anymore

--- src/app/layout.js
+++ src/app/layout.js
@@ -4,7 +4,7 @@ import './globals.css'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Amritesh&apos;s Portfolio',
+  title: "Amritesh's Portfolio",
   description: 'Portfolio made using Next.js',
 }
 
